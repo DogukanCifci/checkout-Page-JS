e269233bcdc0432977ac0bfda9f07b22ef11782f
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-29 12:22:19 +0200

    price and totalprice added

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ sepettekiler.forEach((urun) => {
   <div class="row g-0">
 
     <div class="col-md-5">
-      <img src=${img}    class="img-fluid rounded-start" alt="...">
+      <img src=${img} class="img-fluid rounded-start" alt="...">
     </div>
 
     <div class="col-md-7">
@@ -103,7 +103,55 @@ function removeSil(ürünBtn) {
       urun.name != ürünBtn.closest(".card").querySelector("h5").textContent //.card mainine ulastim ve onun icindeki h5 e ulas dedim. document.querySelector ile ayni. coument tüm js kodu. document yerine article icindeki gibi article.querySlector vb yazabiliriz.
     //Yani burda dedik ki tikladigimiz remove butonunun parentlerindan en yakin card classli elementte git. O elemanin altindan h5 tagine sahip olan elementin icerigine bak. sectigimn ürünün ismi onunla esit degilse o listede kalsin. Esitse onu listeden sil
   );
-  console.log(sepettekiler);
+  console.log(sepettekiler); //Listeden eksiliyor mu diye kontrol etmek icin
+}
+
+//===================== ADET DÜZENLEME========================
+
+adetButton();
+
+function adetButton() {
+  document.querySelectorAll(".adet-controller").forEach((kutu) => {
+    //adet-controller classli elementim var. + - ve adet belirten div. Onun icinden alttaki butonlara erisiyorum. Daha sonra bunlara tiklandiginda söyle olsun böyle olsun diye komut vericem
+    //Birden fazla ürün oldugu icin hangisine tiklandigini algilamak amaciyla querySelectorAll ile hangisine tiklandigini kontrol ettik.
+
+    const eksi = kutu.firstElementChild; //Bu sekilde eksi butonuna ulasmis olduk.Bunun yerine kutu.querySelector(".fa-minus") ' da diyebilirdim.Ama firstElementChild kendinin ilk cocugunu cagirir.Eksi butonu da ilk element oldugu icin bunu kullanabildik
+    let adet = kutu.querySelector("#ürün-adet");
+
+    const arti = kutu.lastElementChild; //Bu da .adet-controller classli elementin son cocugu' da + butonu
+
+    eksi.addEventListener("click", () => {
+      adet.textContent = adet.textContent - 1;
+      //sepette de degistiricez
+      sepettekiler.map((urun) => {
+        if (
+          urun.name == adet.closest(".card").querySelector("h5").textContent //hangisine tiklandiysa sadece onun adet degistirmek icin isimleri eslestirmek zorundayiz
+        ) {
+          urun.adet--;
+        }
+      });
+    });
+    arti.onclick = () => {
+      adet.textContent = Number(adet.textContent) + 1;
+
+      //sepettekini artirma
+      sepettekiler.map((ürün) => {
+        if (
+          ürün.name == adet.closest(".card").querySelector("h5").textContent //hangisine tiklandiysa sadece onun adet degistirmek icin isimleri eslestirmek zorundayiz
+        ) {
+          ürün.adet++;
+        }
+
+        //----Ürün toplami ekrana bastirma
+        let ürünToplam = arti.closest(".card").querySelector(".ürün-toplam");
+        let ürünPrice = arti
+          .closest(".card")
+          .querySelector(".indirim-price").textContent;
+
+        ürünToplam.textContent = (adet.textContent * ürünPrice).toFixed(2);
+      });
+    };
+  });
 }
 
 console.log(sepettekiler);
